c19f1380eed38a6a78155b197c2fa591cf15d445
Dominic Beger <info@dominic-beger.de>
2016-03-12 18:10:10 +0100

    Add PointEnumerator

---
+++ SharpMath/Geometry/PointEnumerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SharpMath.Geometry
+{
+    public class PointEnumerator : IEnumerator<double>
+    {
+        private readonly Point _point;
+        private int _index = -1;
+
+        internal PointEnumerator(Point point)
+        {
+            _point = point;
+        }
+
+        public double Current => _point[(uint) _index];
+
+        object IEnumerator.Current => _point[(uint) _index];
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            _index++;
+            return _index < _point.Dimension;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+    }
+}

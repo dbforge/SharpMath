0af1431773dec1e4dfe73768ff924a4cd126aa39
Dominic Beger <info@dominic-beger.de>
2016-03-17 17:29:39 +0100

    Add VectorExtensions class

---
+++ SharpMath/Geometry/VectorExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace SharpMath.Geometry
+{
+    public static class VectorExtensions
+    {
+        public static T Negate<T>(this Vector vector) where T : Vector, new()
+        {
+            var resultVector = new T();
+            for (uint i = 0; i < vector.Dimension; ++i)
+                resultVector[i] = -vector[i];
+            return resultVector;
+        }
+
+        /// <summary>
+        ///     Calculates the normalized <see cref="Vector"/> of this <see cref="Vector"/>.
+        /// </summary>
+        /// <returns>The normalized <see cref="Vector"/>.</returns>
+        public static T Normalize<T>(this Vector vector) where T : Vector, new()
+        {
+            var resultVector = new T();
+            for (uint i = 0; i < vector.Dimension; ++i)
+                resultVector[i] /= vector.Magnitude;
+            return resultVector;
+        }
+    }
+}

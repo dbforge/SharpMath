6e78219dccf05bc07b22b2dd7d938d0549b7a814
Dominic Beger <admin@trade-programming.de>
2016-03-19 23:52:49 +0100

    Fix normalizing a vector results in a zero vector

--- SharpMath/Geometry/VectorExtensions.cs
+++ SharpMath/Geometry/VectorExtensions.cs
@@ -1,10 +1,15 @@
-﻿namespace SharpMath.Geometry
+﻿using SharpMath.Geometry.Exceptions;
+
+namespace SharpMath.Geometry
 {
     public static class VectorExtensions
     {
         public static T Negate<T>(this Vector vector) where T : Vector, new()
         {
             var resultVector = new T();
+            if (vector.Dimension != resultVector.Dimension)
+                throw new DimensionException("The dimensions of the vectors do not equal each other.");
+
             for (uint i = 0; i < vector.Dimension; ++i)
                 resultVector[i] = -vector[i];
             return resultVector;
@@ -17,8 +22,11 @@
         public static T Normalize<T>(this Vector vector) where T : Vector, new()
         {
             var resultVector = new T();
+            if (vector.Dimension != resultVector.Dimension)
+                throw new DimensionException("The dimensions of the vectors do not equal each other.");
+
             for (uint i = 0; i < vector.Dimension; ++i)
-                resultVector[i] /= vector.Magnitude;
+                resultVector[i] = vector[i] / vector.Magnitude;
             return resultVector;
         }
     }

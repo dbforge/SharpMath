ecdfea6d796745901abc818bcab52aa38fbcbfd3
Dominic Beger <info@dominic-beger.de>
2016-05-08 19:00:37 +0200

    Add example images for canvas

---
+++ SharpMath.CanvasTest/Canvas2DTest.cs
@@ -0,0 +1,24 @@
+﻿// Author: Dominic Beger (Trade/ProgTrade) 2016
+
+using System;
+using System.Windows.Forms;
+using SharpMath.Geometry;
+using SharpMath.Presentation;
+
+namespace SharpMath.Canvas2DTest
+{
+    public partial class Canvas2DTest : Form
+    {
+        public Canvas2DTest()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas2DTest_Load(object sender, EventArgs e)
+        {
+            testCanvas.Functions.Add(new FunctionWrapper(x => Math.Pow(x, 3)));
+            testCanvas.Functions.Add(new FunctionWrapper(x => 1.5 * Math.Sin(x)));
+            testCanvas.Vertices.Add(new Vector2(4, 8));
+        }
+    }
+}

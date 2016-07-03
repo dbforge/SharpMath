0074b2b23f0cab04709ccbb0374287b7b041614e
Dominic Beger <info@dominic-beger.de>
2016-07-03 02:10:54 +0200

    Fix parsing not working correctly with all terms

--- SharpMath/Algorithms.cs
+++ SharpMath/Algorithms.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.Generic;
 using SharpMath.Expressions;
+using SharpMath.Expressions.Exceptions;
 
 namespace SharpMath
 {
@@ -20,8 +21,28 @@ namespace SharpMath
             var opStack = new Stack<Token<string>>();
             var output = new List<Token>(infixTokens.Count);
 
+            Token lastToken = null;
             foreach (var token in infixTokens)
             {
+                if (lastToken != null)
+                {
+                    // A constant value or number must be followed by an operator or closing bracket...
+                    if ((lastToken.Type == TokenType.Constant || lastToken.Type == TokenType.Number) &&
+                        (token.Type != TokenType.Operator && !token.IsClosingBracket()))
+                    {
+                        throw new ParserException(
+                            "Cannot calculate the postfix tokens of the given input as the term is invalid. A constant or number must be followed by an operator or a closing bracket.");
+                    }
+
+                    // A closing bracket must be followed by an operator or another closing bracket...
+                    if (lastToken.IsClosingBracket() && (token.Type != TokenType.Operator && !token.IsClosingBracket()))
+                    {
+                        throw new ParserException(
+                            "Cannot calculate the postfix tokens of the given input as the term is invalid. A closing bracket must be followed by an operator or another closing bracket.");
+                    }
+                }
+
+                lastToken = token;
                 switch (token.Type)
                 {
                     case TokenType.Number:
@@ -33,8 +54,8 @@ namespace SharpMath
                     case TokenType.Operator:
                         Token<string> currentOperatorToken = (Token<string>) token;
                         while (opStack.Count > 0 && opStack.Peek().Type == TokenType.Operator &&
-                               !opStack.Peek().IsRightAssociative &&
-                               currentOperatorToken.Priority <= opStack.Peek().Priority)
+                               (!currentOperatorToken.IsRightAssociative &&
+                               (currentOperatorToken.Priority == opStack.Peek().Priority) || currentOperatorToken.Priority < opStack.Peek().Priority))
                             output.Add(opStack.Pop());
                         opStack.Push(currentOperatorToken);
                         break;
---
+++ SharpMath/Expressions/TokenEx.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpMath.Expressions
+{
+    public static class TokenEx
+    {
+        public static bool IsOpeningBracket(this Token token)
+        {
+            return token.Type == TokenType.Bracket && ((Token<string>)token).Value == "(";
+        }
+
+        public static bool IsClosingBracket(this Token token)
+        {
+            return token.Type == TokenType.Bracket && ((Token<string>) token).Value == ")";
+        }
+    }
+}
